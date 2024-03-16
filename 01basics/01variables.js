c16a77d258a82a7e1a39beed9445823716e41441
Premveer Singh <prem.veer2836@gmail.com>
2024-03-16 06:27:22 +0000

    startnow

---
+++ 01basics/01variables.js
@@ -0,0 +1,13 @@
+const accountID= 423672
+let accountEmail = 'prem@gmail.com'
+var accountPassword = '4321abcd'
+accountCity = "delhi"
+let accountState;
+
+accountEmail='premveer@google.com'
+accountPassword='1234abcd'
+accountCity= 'babaiyan'
+
+
+
+console.table([accountID,accountEmail,accountPassword,accountCity,accountState]);
