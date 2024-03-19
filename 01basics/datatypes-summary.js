688815bc0ecce2d9b13a05f7b6e80c6e5982d138
PremveerSingh195 <prem.veer2836@gmail.com>
2024-03-19 23:39:42 +0530

    sagvc

--- 01basics/datatypes-summary.js
+++ 01basics/datatypes-summary.js
@@ -37,5 +37,31 @@ const myFunc = function () {
     console.log("hello world");
 }
 
-console.log(typeof bigNumber);
+// console.log(typeof bigNumber);
 
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// stack (Primitive), Heap(Non-Primitive)
+
+let myName = "premdotcom"
+
+let anotherName = myName
+
+anotherName = "chaiaurcode"
+
+console.log(myName);
+console.log(anotherName);
+
+let userOne = {
+    email : 'googledotcom',
+    upi: 'user1ybl'
+}
+
+let userTwo = userOne
+
+userTwo.email = 'metadotcom'
+
+console.log(userOne.email);
+console.log(userTwo.email);
