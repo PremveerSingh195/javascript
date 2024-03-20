2a400419e08d1d53a506876de4cbcd924862935a
PremveerSingh195 <prem.veer2836@gmail.com>
2024-03-20 22:19:04 +0530

    csafxc

--- 01basics/07datesinJs.js
+++ 01basics/07datesinJs.js
@@ -11,10 +11,17 @@ let myDate = new Date()
 
 // let createdDate = new Date(2022 , 11 , 1)
 let createdDate = new Date("2001-12-12")
-console.log(createdDate.toLocaleDateString());
+// console.log(createdDate.toLocaleDateString());
 
 const myTimeStamp = Date.now()
 
 
-console.log(myTimeStamp);
-console.log(createdDate);
+// console.log(myTimeStamp);
+// console.log(createdDate.getTime());
+
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date ()
+// console.log(newDate.getDay());
+
+console.log(newDate.toLocaleString('default',{weekday:"short"}));
