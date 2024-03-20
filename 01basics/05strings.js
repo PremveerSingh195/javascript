12188931065e0d8e284d3397dbac57016bc5f995
Premveer Singh <prem.veer2836@gmail.com>
2024-03-20 05:57:24 +0000

    dhsgv

--- 01basics/05strings.js
+++ 01basics/05strings.js
@@ -5,4 +5,33 @@ let position = "Software developer"
 
 console.log(`My name is ${name} and i am a ${position}`);
 
+const gamename = new String('Prem-veerS')
+
+console.log(gamename[0]);
+console.log(gamename.__proto__);
+
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
+console.log(gamename.charAt(4));
+console.log(gamename.indexOf('e'));
+
+const newString = gamename.substring(0,4)
+console.log(newString);
+
+const anotherString = gamename.slice(-8,4)
+
+console.log(anotherString);
+
+const anotherOneString = "     Premone    "
+
+console.log(anotherOneString);
+console.log(anotherOneString.trim());
+
+const url = "www.price404.com/flip%5kart"
+
+console.log(url.replace('%5' , '-'));
+
+console.log(url.includes('dsv'));
+
+console.log(gamename.split('-'));
 
