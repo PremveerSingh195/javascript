911378ef99937d2fa9753e7ed73390c3f6d275ea
Premveer Singh <prem.veer2836@gmail.com>
2024-03-20 16:26:40 +0000

    sdcsd

---
+++ 01basics/07datesinJs.js
@@ -0,0 +1,20 @@
+let myDate = new Date()
+
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.getDate());
+// console.log(myDate.getHours());
+// console.log(myDate.getTimezoneOffset());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleTimeString());
+
+
+// let createdDate = new Date(2022 , 11 , 1)
+let createdDate = new Date("2001-12-12")
+console.log(createdDate.toLocaleDateString());
+
+const myTimeStamp = Date.now()
+
+
+console.log(myTimeStamp);
+console.log(createdDate);
