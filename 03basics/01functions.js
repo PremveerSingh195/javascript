91751530530783f0677e89011566f183792ccd82
Premveer Singh <prem.veer2836@gmail.com>
2024-03-22 06:13:26 +0000

    dvgcsa

--- 03basics/01functions.js
+++ 03basics/01functions.js
@@ -10,3 +10,34 @@ function sayMyName() {
 // sayMyName()
 // sayMyName()
 // sayMyName()
+
+// function addTwoNumbers(num1 ,num2) {
+//   console.log(num1 + num2);
+// }
+
+function addTwoNumbers(num1 ,num2) {
+  // let result = num1 + num2
+  // return result
+  return num1+num2
+}
+ 
+
+const result =  addTwoNumbers(7,6)
+
+// console.log(`Result : ${result}`);
+
+// function loginUserMessage(username) {
+//      return `${username} just logged in`
+// }
+
+function loginUserMessage(username = sam) {
+  if (!username) {
+    console.log("Please entered username");
+    return
+  }
+  return `${username} just logged in`
+}
+
+
+// console.log(loginUserMessage("Premveer"));
+console.log(loginUserMessage("hitesh"));
