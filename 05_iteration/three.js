48128c63d9da7a4ce9437187e773eebca743a71b
PremveerSingh195 <prem.veer2836@gmail.com>
2024-03-26 23:53:29 +0530

    vscdhs

---
+++ 05_iteration/three.js
@@ -0,0 +1,16 @@
+const arr = [1,2,3,4,5]
+
+
+for (const iterator of arr) {
+   // console.log(iterator);
+}
+
+const greetings = "Hello world"
+
+
+for (const iterator of greetings) {
+    if (iterator === " ") {
+    break
+    }
+         console.log(iterator);   // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> for of loop
+}
