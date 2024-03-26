343d577cace87c287b5c3a7a4c0ad25652654977
PremveerSingh195 <prem.veer2836@gmail.com>
2024-03-26 23:21:52 +0530

    dswfd

---
+++ 05_iteration/two.js
@@ -0,0 +1,25 @@
+// let index = 0
+
+// while (index <= 10 ) {
+//     console.log(`value of index is ${index}`);
+//    index = index + 2
+// }
+
+
+const myArray = ["batman" , "superman" , "undertaker"]
+
+let arr = 0
+while (arr < myArray.length) {
+    //console.log(`super hero ${myArray[arr]}`);
+    arr++
+}
+
+
+
+
+let score = 20
+
+do {
+    console.log(`my score is ${score}`);
+    score++
+} while (score <= 10);
