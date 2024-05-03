c9584e1240031abe8d43afac55172621a3cb7592
Premveer Singh <prem.veer2836@gmail.com>
2024-05-03 08:17:57 +0000

    dgsds

--- 10classes_and_OOP/mathpi.js
+++ 10classes_and_OOP/mathpi.js
@@ -3,14 +3,24 @@ const descriptor = Object.getOwnPropertyDescriptor(Math , "PI")
 // console.log(descriptor);
 
 const myObj = {
-    username : "Chai"
+    username : "Chai",
+    price:250,
+    orderchai : function (){
+           console.log("chai nhi bni");
+    }
 }
 
-console.log(Object.getOwnPropertyDescriptor(myObj , "username"));
+// console.log(Object.getOwnPropertyDescriptor(myObj , "username"));
 
-Object.defineProperty(myObj , 'chai' , {
-    writable: false,
+Object.defineProperty(myObj , 'username' , {
+    // writable: false,
     enumerable: false
 })
 
-console.log(Object.getOwnPropertyDescriptor(myObj , "username"));
+// console.log(Object.getOwnPropertyDescriptor(myObj , "username"));
+
+for (let [key , value] of Object.entries(myObj)) {
+  if (typeof value !== 'function') {
+    console.log(`${key}:${value}`);
+  }
+}
